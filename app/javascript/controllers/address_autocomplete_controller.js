20a74edadabd6f78b450e55691a0bfe188eb3e8f
Federico <federico.capaldo@hotmail.com>
2023-11-22 03:32:02 +0000

    hiding previous address field and styled autocomplete seamlessly in form

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -14,9 +14,17 @@ export default class extends Controller {
     })
     this.geocoder.addTo(this.element)
 
-
+    this.geocoder.on("result", event => this.#setInputValue(event))
+    this.geocoder.on("clear", () => this.#clearInputValue())
   }
 
+  #setInputValue(event) {
+    this.addressTarget.value = event.result["place_name"]
+  }
+
+  #clearInputValue() {
+    this.addressTarget.value = ""
+  }
 
   disconnect() {
     this.geocoder.onRemove()

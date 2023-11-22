e3f8cc7df82eb8d1fb53f70e142edcf748a5dac9
Federico <federico.capaldo@hotmail.com>
2023-11-22 02:57:12 +0000

    adding markers to map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -4,16 +4,29 @@ import mapboxgl from "mapbox-gl"
 // Connects to data-controller="map"
 export default class extends Controller {
   static values = {
-    apiKey: String
+    apiKey: String,
+    markers: Array
   }
 
   connect() {
-    console.log(this.apiKeyValue)
+    this.#loadMap()
+    this.#addMarkersToMap()
+  }
+
+  #loadMap() {
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
+    })
+  }
+
+  #addMarkersToMap () {
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([marker.lon, marker.lat])
+        .addTo(this.map)
     })
   }
 }

30ec67fd9f0b4977169c0de0de920e66fd378683
Federico <federico.capaldo@hotmail.com>
2023-11-22 03:19:01 +0000

    going futher: another example of a custom marker (blue round pill)

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -9,7 +9,7 @@ class FlatsController < ApplicationController
       lat: flat.latitude,
       lon: flat.longitude,
       info_window_html: render_to_string(partial: "info_window", locals: {flat: flat}),
-      marker_html: render_to_string(partial: "marker")
+      marker_html: render_to_string(partial: "marker", locals: {flat: flat})
     }
     end
   end
